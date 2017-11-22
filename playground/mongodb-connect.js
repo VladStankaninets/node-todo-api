1c0971e62adb570fde43b201f0031c3ccbd9aa93
Vlad Stankaninets <vlad.stankaninets@gmail.com>
2017-11-22 11:52:00 -0500

    First Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,37 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb'); // New ES6 Syntax - Object Destructuring
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (ConnectErr, db) => {
+
+    if (ConnectErr) console.log('Unable to connect');
+    else console.log('Connected to MongoDB Server');
+
+    // db.collection('ToDos').insertOne(
+    //     {'text': 'Someting to do', 'completed': false},
+    //     (InsertErr, result) => {
+    //         if (InsertErr) return console.log('Unable to insert todo', InsertErr);
+    //         // else
+    //         console.log(JSON.stringify(result.ops, undefined, 2));
+    //     }
+    // );
+    //
+    // db.collection('Users').insertOne(
+    //     {
+    //         'Name': 'Vlad',
+    //         'Age': 96,
+    //         'Location': 'Toronto'
+    //     },
+    //     (InsertErr, result) => {
+    //         if (InsertErr) return console.log('Unable to insert todo', InsertErr);
+    //         // else
+    //         // console.log(JSON.stringify(result.ops, undefined, 2));
+    //         console.log(result.ops[0]._id.getTimestamp());
+    //     }
+    //
+    // );
+
+    db.close();
+});

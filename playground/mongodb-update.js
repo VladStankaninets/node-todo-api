0b0fba28ded98c84889df36793aba152c2653cea
Vlad Stankaninets <vlad.stankaninets@gmail.com>
2017-11-24 14:41:23 -0500

    There is insert and update in this file. Connecting to mLAB MongoDB server

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -6,17 +6,47 @@ MongoClient.connect('mongodb://vlad:vlad123@ds117336.mlab.com:17336/vladdb', (Co
     if (ConnectErr) console.log('Unable to connect to MLAB MongoDB Server');
     else console.log('Connected to MLAB MongoDB Server');
 
-    db.collection('ToDos').insertOne(
-        {
-            'text': 'Something to do',
-            'completed': false
-        },
-        (InsertErr, result) => {
-            if (InsertErr) return console.log('Unable to insert todo', InsertErr);
-            // else
-            console.log(JSON.stringify(result.ops, undefined, 2));
-        }
-    );
+    // db.collection('ToDos').findOneAndUpdate({
+    //     '_id': new ObjectID('5a1792c4b6f73c2a745a51c0')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // })
+    //     .then((result) => {
+    //         console.log(result);
+    //     });
 
-    db.close();
+    db.collection('Users').findOneAndUpdate({
+        // '_id': new ObjectID('5a186e03ae9f375d1049849c')
+        'name': 'Mike'
+    }, {
+        $inc: {
+            'age': 1
+        },
+        $set: {
+            'name': 'Vlad'
+              }
+        }, {
+            returnOriginal: false
+    })
+        .then((result) => {
+            console.log(result);
+        });
+
+    // db.collection('ToDos').insertOne(
+    //     {
+    //         'text': 'Something to do',
+    //         'completed': false
+    //     },
+    //     (InsertErr, result) => {
+    //         if (InsertErr) return console.log('Unable to insert todo', InsertErr);
+    //         // else
+    //         console.log(JSON.stringify(result.ops, undefined, 2));
+    //     }
+    // );
+
+    // db.close();
 });

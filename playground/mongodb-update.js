0f78a0928aa2f21a7ae7b27f1af9dc58e432e3a7
Vlad Stankaninets <vlad.stankaninets@gmail.com>
2017-11-24 22:15:41 -0500

    Updated URL param in the config file

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -1,7 +1,7 @@
 // const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb'); // New ES6 Syntax - Object Destructuring
-
-MongoClient.connect('mongodb://vlad:vlad123@ds117336.mlab.com:17336/vladdb', (ConnectErr, db) => {
+const config = require('../config.js');
+MongoClient.connect(config.URL, (ConnectErr, db) => {
 
     if (ConnectErr) console.log('Unable to connect to MLAB MongoDB Server');
     else console.log('Connected to MLAB MongoDB Server');
@@ -19,34 +19,34 @@ MongoClient.connect('mongodb://vlad:vlad123@ds117336.mlab.com:17336/vladdb', (Co
     //         console.log(result);
     //     });
 
-    db.collection('Users').findOneAndUpdate({
-        // '_id': new ObjectID('5a186e03ae9f375d1049849c')
-        'name': 'Mike'
-    }, {
-        $inc: {
-            'age': 1
-        },
-        $set: {
-            'name': 'Vlad'
-              }
-        }, {
-            returnOriginal: false
-    })
-        .then((result) => {
-            console.log(result);
-        });
-
-    // db.collection('ToDos').insertOne(
-    //     {
-    //         'text': 'Something to do',
-    //         'completed': false
+    // db.collection('Users').findOneAndUpdate({
+    //     // '_id': new ObjectID('5a186e03ae9f375d1049849c')
+    //     'name': 'Mike'
+    // }, {
+    //     $inc: {
+    //         'age': 1
     //     },
-    //     (InsertErr, result) => {
-    //         if (InsertErr) return console.log('Unable to insert todo', InsertErr);
-    //         // else
-    //         console.log(JSON.stringify(result.ops, undefined, 2));
-    //     }
-    // );
+    //     $set: {
+    //         'name': 'Vlad'
+    //           }
+    //     }, {
+    //         returnOriginal: false
+    // })
+    //     .then((result) => {
+    //         console.log(result);
+    //     });
+
+    db.collection('ToDos').insertOne(
+        {
+            'text': 'Something to do',
+            'completed': false
+        },
+        (InsertErr, result) => {
+            if (InsertErr) return console.log('Unable to insert todo', InsertErr);
+            // else
+            console.log(JSON.stringify(result.ops, undefined, 2));
+        }
+    );
 
     // db.close();
 });

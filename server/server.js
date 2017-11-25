b05c8c1d229cd97a1b0c4aecce60e0df13ece5f0
Vlad Stankaninets <vlad.stankaninets@gmail.com>
2017-11-25 17:44:57 -0500

    Closing connection with mongoose.disconnect()

--- server/server.js
+++ server/server.js
@@ -2,6 +2,35 @@ const {mongoose} = require('./db/mongoose.js');
 const {ToDo} = require('./models/todo.js');
 const {User} = require('./models/user.js');
 
+// To handle HTTP POST request in Express.js version 4 and above, you need to install middleware module called body-parser.
+// Body-parser extract the entire body portion of an incoming request stream and exposes it on req.body
+// The middleware was a part of Express.js earlier but now you have to install it separately.
+// This body-parser module parses the JSON, buffer, string and url encoded data submitted using HTTP POST request
+// This is usually necessary when you need to know more than just the URL you hit,
+// particularly in the context of a POST or PUT PATCH HTTP request where the information you want is contains in the body.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // create a brand new ToDo
 // var newToDo = new ToDo({'text': 'Cook dinner'});
@@ -30,13 +59,14 @@ const {User} = require('./models/user.js');
 //     console.log('Unable to save a newTodo2', err);
 // });
 //
-// var newUser = new User({
-//     'name': '   Vlad Stankaninets   ',
-//     'e-mail': 'vlad.s@example.com        '
-// });
-//
-// newUser.save().then((doc) => {
-//     console.log('Saved a new User', doc);
-// }, (err) => {
-//     console.log('Unable to save a new User', err);
-// });
+var newUser = new User({
+    'name': '   P Stankaninets   ',
+    'e-mail': 'p.s@example.com        '
+});
+
+newUser.save().then((doc) => {
+    console.log('Saved a new User', doc);
+    mongoose.disconnect();
+}, (err) => {
+    console.log('Unable to save a new User', err);
+});

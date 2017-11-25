70d197face3e8929663b082428d3c73db0cfeafe
Vlad Stankaninets <vlad.stankaninets@gmail.com>
2017-11-24 22:15:41 -0500

    Updated URL param in the config file

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const config = require('./config.js');
+const config = require('../config.js');
 // Mongoose uses callbacks by default and we would want to use Promises
-// So, we need to instruct mongoose to use Promises built-in library (part of teh JS now)
-// as opposed to 3-rd party Promises library as blubird from pre-ES6 days
+// So, we need to instruct mongoose to use Promises built-in library (part of JS now)
+// as opposed to 3-rd party Promises library as bluebird from pre-ES6 days
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://vlad:vlad123@ds117336.mlab.com:17336/vladdb');
-mongoose.connect(`mongodb://${config.user}:${config.password}@ds117336.mlab.com:17336/vladdb`, {
+mongoose.connect(config.URL, {
     'useMongoClient': true
     // other options
 });
